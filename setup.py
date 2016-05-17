e47ef84efc374e87827f5b5b598a25607adec0c1
Yiyin Zhou <yiyin@ee.columbia.edu>
2016-05-17 10:57:07 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys, os
+from glob import glob
+
+# Install setuptools if it isn't available:
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+from distutils.command.install_headers import install_headers
+from setuptools import find_packages
+from setuptools import setup
+
+NAME =               'ypcutil'
+VERSION =            '0.1'
+AUTHOR =             'Yiyin Zhou'
+AUTHOR_EMAIL =       'yiyin@columbia.edu'
+MAINTAINER =         AUTHOR
+MAINTAINER_EMAIL =   AUTHOR_EMAIL
+DESCRIPTION =        'Utilities Functions with Pitch Arrays'
+LONG_DESCRIPTION =   DESCRIPTION
+LICENSE =            'BSD'
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development']
+
+# Explicitly switch to parent directory of setup.py in case it
+# is run from elsewhere:
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+PACKAGES =           find_packages()
+
+if __name__ == "__main__":
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    setup(
+        name = NAME,
+        version = VERSION,
+        author = AUTHOR,
+        author_email = AUTHOR_EMAIL,
+        license = LICENSE,
+        classifiers = CLASSIFIERS,
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        maintainer = MAINTAINER,
+        maintainer_email = MAINTAINER_EMAIL,
+        packages = PACKAGES,
+        include_package_data = True,
+        )
